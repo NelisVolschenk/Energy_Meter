6c5feafb42affe80da231b68963dd98c0d77c263
Nelis <nelisvolschenk@gmail.com>
2018-05-26 12:15:51 +0200

    Gone to barebones

--- Energy_Meter.ino
+++ Energy_Meter.ino
@@ -236,111 +236,6 @@ void pllcalcs (int NewV1){
 }
 
 
-void addcycle () {
-
-    TotalV1Squared += CycleV1Squared;
-    TotalV2Squared += CycleV2Squared;
-    TotalV3Squared += CycleV3Squared;
-    TotalI1Squared += CycleI1Squared;
-    TotalI2Squared += CycleI2Squared;
-    TotalI3Squared += CycleI3Squared;
-    if (CycleP1>=0){
-        TotalP1Import += CycleP1;
-    } else {
-        TotalP1Export -= CycleP1;
-    }
-    if (CycleP2>=0){
-        TotalP2Import += CycleP2;
-    } else {
-        TotalP2Export -= CycleP2;
-    }
-    if (CycleP3>=0){
-        TotalP3Import += CycleP3;
-    } else {
-        TotalP3Export -= CycleP3;
-    }
-    SumTimerCount += (OCR1A+1);
-
-    CycleCount++;
-    NewCycle = false;
-}
-
-
-void calculateVIPF(){
-    //todo VOltage range
-
-    float TotalTime=0;
-
-    V1rms = V1RATIO * sqrt(((float)TotalV1Squared) / LOOPSAMPLES);
-    V2rms = V2RATIO * sqrt(((float)TotalV2Squared) / LOOPSAMPLES);
-    V3rms = V3RATIO * sqrt(((float)TotalV3Squared) / LOOPSAMPLES);
-    I1rms = I1RATIO * sqrt(((float)TotalI1Squared) / LOOPSAMPLES);
-    I2rms = I2RATIO * sqrt(((float)TotalI2Squared) / LOOPSAMPLES);
-    I3rms = I3RATIO * sqrt(((float)TotalI3Squared) / LOOPSAMPLES);
-
-    RealPower1Import = (V1RATIO * I1RATIO * (float)TotalP1Import) / LOOPSAMPLES;
-    RealPower2Import = (V2RATIO * I2RATIO * (float)TotalP2Import) / LOOPSAMPLES;
-    RealPower3Import = (V3RATIO * I3RATIO * (float)TotalP3Import) / LOOPSAMPLES;
-    RealPower1Export = (V1RATIO * I1RATIO * (float)TotalP1Export) / LOOPSAMPLES;
-    RealPower2Export = (V2RATIO * I2RATIO * (float)TotalP2Export) / LOOPSAMPLES;
-    RealPower3Export = (V3RATIO * I3RATIO * (float)TotalP3Export) / LOOPSAMPLES;
-    ApparentPower1 = V1rms * I1rms;
-    ApparentPower2 = V2rms * I2rms;
-    ApparentPower3 = V3rms * I3rms;
-    PowerFactor1 = (RealPower1Import + RealPower1Export) / ApparentPower1;
-    PowerFactor2 = (RealPower2Import + RealPower2Export) / ApparentPower2;
-    PowerFactor3 = (RealPower3Import + RealPower3Export) / ApparentPower3;
-
-    TV1 = RealPower1Import;
-    TV2 = RealPower1Export;
-    TV3 = ApparentPower1;
-
-    TotalTime = ((float)SumTimerCount * NUMSAMPLES) / AVRCLOCKSPEED; // Time in seconds
-    Frequency = (float)CycleCount / TotalTime;
-
-    // Calcualte the units used, 0.5 added for correct rounding
-    UnitsUsed1 = long((RealPower1Import * TotalTime / 3.6) + 0.5);
-    UnitsUsed2 = long((RealPower2Import * TotalTime / 3.6) + 0.5);
-    UnitsUsed3 = long((RealPower3Import * TotalTime / 3.6) + 0.5);
-
-    // Update The unit counter
-    Units1 -= UnitsUsed1;
-    Units2 -= UnitsUsed2;
-    Units3 -= UnitsUsed3;
-
-    // Clear the counters
-    TotalV1Squared = 0;
-    TotalV2Squared = 0;
-    TotalV3Squared = 0;
-    TotalI1Squared = 0;
-    TotalI2Squared = 0;
-    TotalI3Squared = 0;
-    TotalP1Import = 0;
-    TotalP2Import = 0;
-    TotalP3Import = 0;
-    TotalP1Export = 0;
-    TotalP2Export = 0;
-    TotalP3Export = 0;
-    SumTimerCount = 0;
-    CycleCount = 0;
-
-}
-
-
-void switchrelays (){
-
-    // Set relay states
-    digitalWrite(RELAY1PIN,Relay1State);
-    digitalWrite(RELAY2PIN,Relay2State);
-    digitalWrite(RELAY3PIN,Relay3State);
-    if (PllUnlocked==0) {
-        digitalWrite(PLLLOCKEDPIN, HIGH);
-    } else {
-        digitalWrite(PLLLOCKEDPIN,LOW);
-    }
-
-}
-
 void sendjson(int vadc, int iadc){
 
     StaticJsonBuffer<100> jsonBuffer;
@@ -353,65 +248,6 @@ void sendjson(int vadc, int iadc){
 
     JsonOutput.printTo(Serial);
     Serial.println();
-
-}
-
-void sendresults(){
-    // Radio communication
-    // todo Radio communication to raspberry PI
-    /*
-    StaticJsonBuffer<800> jsonBuffer;
-    JsonObject& JsonOutput = jsonBuffer.createObject();
-
-    JsonOutput["ID"] = DEVICEID;
-    JsonOutput["Frequency"] = Frequency;
-    JsonOutput["PLL"] = PllUnlocked;
-    JsonOutput["V1"] = V1rms;
-    JsonOutput["I1"] = I1rms;
-    JsonOutput["PowerFactor1"] = PowerFactor1;
-    JsonOutput["PImport1"] = RealPower1Import;
-    JsonOutput["PExport1"] = RealPower1Export;
-    JsonOutput["UnitsUsed1"] = UnitsUsed1;
-    JsonOutput["Units1"] = Units1;
-
-
-
-
-
-    JsonOutput.printTo(Serial);
-    Serial.println();
-
-
-
-
-    Serial.print("V1rms: ");
-    Serial.println(V1rms);
-    Serial.print("I1rms: ");
-    Serial.println(I1rms);
-    Serial.print("Power Factor 1: ");
-    Serial.println(PowerFactor1);
-    Serial.print("Frequency: ");
-    Serial.println(Frequency);
-    Serial.print("PLL: ");
-    Serial.println(PllUnlocked);
-
-
-    Serial.print("Testvalue1: ");
-    Serial.println(TV1);
-    Serial.print("Testvalue2: ");
-    Serial.println(TV2);
-    Serial.print("Testvalue3: ");
-    Serial.println(TV3);
-    Serial.println(" ");
-
-
-    Serial.println("V               I");
-    for (int i = 0; i < NUMSAMPLES; ++i) {
-        Serial.print(V[i]);
-        Serial.print("               ");
-        Serial.println(I[i]);
-    }
-    */
 
 }
 
@@ -449,15 +285,7 @@ void setup() {
 
 
 void loop() {
-    // Calculate averages and transmit data to base station
-    if (NewCycle) {
-        addcycle();
-    }
-    if (CycleCount >= LOOPCYCLES){
-        calculateVIPF();
-        switchrelays();
-        sendresults();
-    }
+
 }
 
 // Timer interrupt
@@ -472,47 +300,14 @@ ISR(ADC_vect){
 
     // Variables that persist between conversions
     static int NewV1=0;
-    static int NewI1=0;
-    static int NewV2=0;
-    static int NewI2=0;
-    static int NewV3=0;
-    static int NewI3=0;
-    static int PrevV1=0;
-    static int PrevV2=0;
-    static int PrevV3=0;
-    static int PrevI1=0;
-    static int PrevI2=0;
-    static int PrevI3=0;
-    static long FilterV1Offset=FILTEROFFSET;
-    static long FilterV2Offset=FILTEROFFSET;
-    static long FilterV3Offset=FILTEROFFSET;
-    static long FilterI1Offset=FILTEROFFSET;
-    static long FilterI2Offset=FILTEROFFSET;
-    static long FilterI3Offset=FILTEROFFSET;
-    static int V1Zero=0;
-    static int V2Zero=0;
-    static int V3Zero=0;
-    static int I1Zero=0;
-    static int I2Zero=0;
-    static int I3Zero=0;
-    static byte V1FilterPoint=0;
-    static byte V2FilterPoint=0;
-    static byte V3FilterPoint=0;
-    static byte I1FilterPoint=0;
-    static byte I2FilterPoint=0;
-    static byte I3FilterPoint=0;
 
     // Temporary for quick serial comm
     static int VAdc=0;
-    static String SerMsg;
 
     // Other variables
     int ADCValue=0;
-    long PhaseShiftedV=0;
-    int TimerNow=0;
 
     // Collect the result from the registers and combine
-    TimerNow = TCNT1;
     ADCValue = ADCL;
     ADCValue |= ADCH<<8;
 
@@ -523,23 +318,7 @@ ISR(ADC_vect){
             ADMUX = _BV(REFS0) | I1PIN; // Set parameters for ADC conversion to start on I1Pin
             ADCSRA |= _BV(ADSC); // Start ADC conversion
             // Update variables for V1
-            PrevV1 = NewV1;
             NewV1 =  ADCValue - V1Offset;
-
-            // Store first positive reading for filter calculations and mark where filter should be updated
-            if ((NewV1>=0)&&(PrevV1<0)) {
-                V1Zero = NewV1;
-                V1FilterPoint = SampleNum + (NUMSAMPLES/2);
-                if (V1FilterPoint>=NUMSAMPLES) {
-                    V1FilterPoint -= NUMSAMPLES;
-                }
-            }
-
-            // Update low pass filter at centre of wave
-            if (SampleNum == V1FilterPoint) {
-                FilterV1Offset += (V1Zero+NewV1)>>1;
-                V1Offset=(int)((FilterV1Offset+FILTERROUNDING)>>FILTERSHIFT);
-            }
 
             VAdc = ADCValue;
 
@@ -549,44 +328,6 @@ ISR(ADC_vect){
         case I1PIN: // I1 Just completed
             ADMUX = _BV(REFS0) | V2PIN; // Set ADC conversion to start on V2Pin
             ADCSRA |= _BV(ADSC); // Start ADC conversion
-            // Update variables for I1
-            PrevI1 = NewI1;
-            NewI1 =  ADCValue - I1Offset;
-
-            /* Store first positive reading for filter calculations and mark where filter should be updated
-            if ((NewI1>=0)&&(PrevI1<0)) {
-                I1Zero = NewI1;
-                I1FilterPoint = SampleNum + (NUMSAMPLES/2);
-                if (I1FilterPoint>=NUMSAMPLES) {
-                    I1FilterPoint -= NUMSAMPLES;
-                }
-            }
-            // Update low pass filter at centre of wave
-            if (SampleNum == I1FilterPoint) {
-                FilterI1Offset += (I1Zero+NewI1)>>1;
-                I1Offset=(int)((FilterI1Offset+FILTERROUNDING)>>FILTERSHIFT);
-            }
-            */
-            FilterI1Offset += (NewI1);  // update the filter
-            I1Offset=(int)((FilterI1Offset+FILTERROUNDING)>>FILTERSHIFT);
-
-            // Apply phase correction and calculate
-            if (I1PhaseShift>=0) { // Current leads voltage: Interpolate voltage to previous current
-                PhaseShiftedV = ((((long)NewV1-PrevV1)*(I1PhaseShift)/TimerCount)) + PrevV1;
-                SumP1 += (PhaseShiftedV*PrevI1);
-                SumV1Squared += (PhaseShiftedV*PhaseShiftedV);
-                SumI1Squared += (PrevI1*PrevI1);
-                //V[SampleNum] = PhaseShiftedV;
-                //I[SampleNum] = PrevI1;
-            }
-            if (I1PhaseShift<0){ // Current lags voltage: Interpolate voltage to new current
-                PhaseShiftedV = ((((long)NewV1-PrevV1)*(I1PhaseShift)/TimerCount)) + NewV1;
-                SumP1 += (PhaseShiftedV*NewI1);
-                SumV1Squared += (PhaseShiftedV*PhaseShiftedV);
-                SumI1Squared += (NewI1*NewI1);
-                //V[SampleNum] = PhaseShiftedV;
-                //I[SampleNum] = NewI1;
-            }
 
             V[SampleNum] = VAdc;
             I[SampleNum] = ADCValue;
@@ -596,135 +337,13 @@ ISR(ADC_vect){
                 for (int i=0; i < NUMSAMPLES; i++){
                     sendjson(V[i],I[i]);
                 }
+                String newline;
+                newline = "{\"V\":\"New\"}";
+                Serial.println(newline);
                 interrupts();
             }
 
-
-
-            break;
-
-        case V2PIN: // V2 Just completed
-            ADMUX = _BV(REFS0) | I2PIN; // Set parameters for ADC conversion to start on I1Pin
-            ADCSRA |= _BV(ADSC); // Start ADC conversion
-            // Update variables for V2
-            PrevV2 = NewV2;
-            NewV2 =  ADCValue - V2Offset;
-            // Store first positive reading for filter calculations and mark where filter should be updated
-            if ((NewV2>=0)&&(PrevV2<0)) {
-                V2Zero = NewV2;
-                V2FilterPoint = SampleNum + (NUMSAMPLES/2);
-                if (V2FilterPoint>=NUMSAMPLES) {
-                    V2FilterPoint -= NUMSAMPLES;
-                }
-            }
-            // Update low pass filter at centre of wave
-            if (SampleNum == V2FilterPoint) {
-                FilterV2Offset += (V2Zero+NewV2)>>1;
-                V2Offset=(int)((FilterV2Offset+FILTERROUNDING)>>FILTERSHIFT);
-            }
-            break;
-
-        case I2PIN: // I2 Just completed
-            ADMUX = _BV(REFS0) | V3PIN; // Set ADC conversion to start on V2Pin
-            ADCSRA |= _BV(ADSC); // Start ADC conversion
-            // Update variables for I2
-            PrevI2 = NewI2;
-            NewI2 =  ADCValue - I2Offset;
-            /* Store first positive reading for filter calculations and mark where filter should be updated
-            if ((NewI2>=0)&&(PrevI2<0)) {
-                I2Zero = NewI2;
-                I2FilterPoint = SampleNum + (NUMSAMPLES/2);
-                if (I2FilterPoint>=NUMSAMPLES) {
-                    I2FilterPoint -= NUMSAMPLES;
-                }
-            }
-            // Update low pass filter at centre of wave
-            if (SampleNum == I2FilterPoint) {
-                FilterI2Offset += (I2Zero+NewI2)>>1;
-                I2Offset=(int)((FilterI2Offset+FILTERROUNDING)>>FILTERSHIFT);
-            }
-            */
-            FilterI1Offset += (NewI1);  // update the filter
-            I1Offset=(int)((FilterI1Offset+FILTERROUNDING)>>FILTERSHIFT);
-            // Apply phase correction and calculate
-            if (I2PhaseShift>=0) { // Current leads voltage: Interpolate voltage to previous current
-                PhaseShiftedV = ((((long)NewV2-PrevV2)*(I2PhaseShift)/TimerCount)) + PrevV2;
-                SumP2 += (PhaseShiftedV*PrevI2);
-                SumV2Squared += (PhaseShiftedV*PhaseShiftedV);
-                SumI2Squared += (PrevI2*PrevI2);
-            }
-            if (I2PhaseShift<0){ // Current lags voltage: Interpolate voltage to new current
-                PhaseShiftedV = ((((long)NewV2-PrevV2)*(I2PhaseShift)/TimerCount)) + NewV2;
-                SumP2 += (PhaseShiftedV*NewI2);
-                SumV2Squared += (PhaseShiftedV*PhaseShiftedV);
-                SumI2Squared += (NewI2*NewI2);
-            }
-            break;
-
-        case V3PIN: // V3 Just completed
-            ADMUX = _BV(REFS0) | I3PIN; // Set parameters for ADC conversion to start on I1Pin
-            ADCSRA |= _BV(ADSC); // Start ADC conversion
-            // Update variables for V2
-            PrevV3 = NewV3;
-            NewV3 =  ADCValue - V3Offset;
-            // Store first positive reading for filter calculations and mark where filter should be updated
-            if ((NewV3>=0)&&(PrevV3<0)) {
-                V3Zero = NewV3;
-                V3FilterPoint = SampleNum + (NUMSAMPLES/2);
-                if (V3FilterPoint>=NUMSAMPLES) {
-                    V3FilterPoint -= NUMSAMPLES;
-                }
-            }
-            // Update low pass filter at centre of wave
-            if (SampleNum == V3FilterPoint) {
-                FilterV3Offset += (V3Zero+NewV3)>>1;
-                V3Offset=(int)((FilterV3Offset+FILTERROUNDING)>>FILTERSHIFT);
-            }
-            break;
-
-        case I3PIN: // I3 Just completed
-            // No new ADC conversion here as this is the last in the list
-
-            // Update variables for I3
-            PrevI3 = NewI3;
-            NewI3 =  ADCValue - I3Offset;
-            /* Store first positive reading for filter calculations and mark where filter should be updated
-            if ((NewI3>=0)&&(PrevI3<0)) {
-                I3Zero = NewI3;
-                I3FilterPoint = SampleNum + (NUMSAMPLES/2);
-                if (I3FilterPoint>=NUMSAMPLES) {
-                    I3FilterPoint -= NUMSAMPLES;
-                }
-            }
-            // Update low pass filter at centre of wave
-            if (SampleNum == I3FilterPoint) {
-                FilterI3Offset += (I3Zero+NewI3)>>1;
-                I3Offset=(int)((FilterI3Offset+FILTERROUNDING)>>FILTERSHIFT);
-            }
-            */
-            FilterI1Offset += (NewI1);  // update the filter
-            I1Offset=(int)((FilterI1Offset+FILTERROUNDING)>>FILTERSHIFT);
-            // Apply phase correction and calculate
-            if (I3PhaseShift>=0) { // Current leads voltage: Interpolate voltage to previous current
-                PhaseShiftedV = ((((long)NewV3-PrevV3)*(I3PhaseShift)/TimerCount)) + PrevV3;
-                SumP3 += (PhaseShiftedV*PrevI3);
-                SumV3Squared += (PhaseShiftedV*PhaseShiftedV);
-                SumI3Squared += (PrevI3*PrevI3);
-            }
-            if (I3PhaseShift<0){ // Current lsgs voltage: Interpolate voltage to new current
-                PhaseShiftedV = ((((long)NewV3-PrevV3)*(I3PhaseShift)/TimerCount)) + NewV3;
-                SumP3 += (PhaseShiftedV*NewI3);
-                SumV3Squared += (PhaseShiftedV*PhaseShiftedV);
-                SumI3Squared += (NewI3*NewI3);
-            }
-            /*
-             * Determine CPU Usage
-            Serial.print("TimerCount: ");
-            Serial.println(TimerCount);
-            Serial.print("TimerNow: ");
-            Serial.println(TimerNow);
-            */
-             pllcalcs(NewV1);
+            pllcalcs(NewV1);
             break;
     }
 
